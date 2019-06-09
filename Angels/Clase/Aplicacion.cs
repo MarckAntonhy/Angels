b888160776c8c6b2c03b5e8b37fd040a7405eb44
Marck <marco98.vega@gmail.com>
2019-06-09 09:26:29 -0500

    Envio de Correo

---
+++ Angels/Clase/Aplicacion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Angels.Clase
+{
+    class Aplicacion
+    {
+        public static string Software = "Software Angel's";
+        public static string Version = "1.0";
+    }
+}
